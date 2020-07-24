b07b3c21456774420bcbd17c8f357b3ac06d71de
DESKTOP-7N7EL58\willda07 <willda07@luther.edu>
2020-07-23 20:58:31 -0500

    Start working on menu

--- project-solar/src/App.vue
+++ project-solar/src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app">
+    <router-view/>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/Map">Map</router-link> |
-      <router-link to="/statistics">Statistics</router-link> |
-      <router-link to="/policies">Policies</router-link> |
+      <router-link to="/">Home</router-link>
+      <router-link to="/Map">Map</router-link>
+      <router-link to="/statistics">Statistics</router-link>
+      <router-link to="/policies">Policies</router-link>
       <router-link to="/info">Info</router-link>
     </div>
-    <router-view/>
   </div>
 </template>
 
@@ -21,11 +21,17 @@
 }
 
 #nav {
-  padding: 30px;
-
+  margin-top: 30px;
   a {
     font-weight: bold;
     color: #2c3e50;
+    border: 5px solid green;
+    border-radius: 5px;
+    padding: 40px;
+    padding-top: 20px;
+    padding-bottom: 50px;
+    text-decoration: none;
+    margin: 10px;
 
     &.router-link-exact-active {
       color: #42b983;
