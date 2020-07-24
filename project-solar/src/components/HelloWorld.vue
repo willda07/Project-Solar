1494281c31e88a8c7f83c26daf77c3740738ab8f
Colin Willenborg <colinwillenborg1414@gmail.com>
2020-07-23 21:40:50 -0400

    cleaning up home page

--- project-solar/src/components/HelloWorld.vue
+++ project-solar/src/components/HelloWorld.vue
@@ -3,19 +3,6 @@
     <h1>{{ msg }}</h1>
     <h2>{{ test }}</h2>
     <button>Info</button>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
   </div>
 </template>
 
@@ -25,7 +12,7 @@ import { Component, Prop, Vue } from 'vue-property-decorator'
 @Component
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
-  public test = 'dab';
+  public test = 'Description of project';
 }
 </script>
 
